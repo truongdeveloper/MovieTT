101ef025c3a0eede8a3722877c8731c4047f0a6d
truongdeveloper <vanminhtruong678@gmail.com>
2023-02-19 04:01:48 +0700

    loading

---
+++ app/src/main/java/com/example/moviett/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.moviett;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash1);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        // Delay to show splash screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Start main activity after delay
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
