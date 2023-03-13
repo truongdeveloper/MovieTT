61b730874ae3222d382c0bc139cb274853a0254b
truong <vanminhtruong678@gmail.com>
2023-03-13 22:07:31 +0700

    Fix Preview

--- app/src/main/java/com/example/moviett/ComingSoon.java
+++ app/src/main/java/com/example/moviett/ComingSoon.java
@@ -1,11 +1,14 @@
 package com.example.moviett;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class ComingSoon extends AppCompatActivity {
 
@@ -15,42 +18,45 @@ public class ComingSoon extends AppCompatActivity {
         setContentView(R.layout.activity_coming_soon);
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.navigation_home: {
-                    // Handle home click
-                    Intent intent = new Intent(this, MainActivity.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                    finish();
-                    return true;
-                }
-                case R.id.navigation_search: {
-                    // Handle favorites click
-                    Intent intent = new Intent(this, MovieSearch.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                    finish();
-                    return true;
-                }
-                case R.id.navigation_trending: {
-                    // Handle settings click
-                    Intent intent = new Intent(this, Trending.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                    finish();
-                    return true;
-                }
-                case R.id.navigation_coming: {
-                    // Handle settings click
-                    Intent intent = new Intent(this, ComingSoon.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                    finish();
-                    return true;
-                }
-            }
-            return false;
-        });
+//        navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()) {
+//                case R.id.navigation_home: {
+//                    // Handle home click
+//                    Intent intent = new Intent(this, MainActivity.class);
+//                    startActivity(intent);
+//                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+//                    finish();
+//                    return true;
+//                }
+//                case R.id.navigation_search: {
+//                    // Handle favorites click
+//                    Intent intent = new Intent(this, MovieSearch.class);
+//                    startActivity(intent);
+//                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+//                    finish();
+//                    return true;
+//                }
+//                case R.id.navigation_trending: {
+//                    // Handle settings click
+//                    Intent intent = new Intent(this, Trending.class);
+//                    startActivity(intent);
+//                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+//                    finish();
+//                    return true;
+//                }
+//                case R.id.navigation_coming: {
+//                    // Handle settings click
+//                    Intent intent = new Intent(this, ComingSoon.class);
+//                    startActivity(intent);
+//                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+//                    finish();
+//                    return true;
+//                }
+//            }
+//                return false;
+//            }
+//        });
     }
 }
--- app/src/main/java/com/example/moviett/MainActivity.java
+++ app/src/main/java/com/example/moviett/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.moviett;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,42 +23,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.navigation_home: {
-                    // Handle home click
-                    Intent intent = new Intent(this, MainActivity.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                    finish();
-                    return true;
+        navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home: {
+                        // Handle home click
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                        finish();
+                        return true;
+                    }
+                    case R.id.navigation_search: {
+                        // Handle favorites click
+                        Intent intent = new Intent(getApplicationContext(), MovieSearch.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                        finish();
+                        return true;
+                    }
+                    case R.id.navigation_trending: {
+                        // Handle settings click
+                        Intent intent = new Intent(getApplicationContext(), Trending.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                        finish();
+                        return true;
+                    }
+                    case R.id.navigation_coming: {
+                        // Handle settings click
+                        Intent intent = new Intent(getApplicationContext(), ComingSoon.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
+                        finish();
+                        return true;
+                    }
                 }
-                case R.id.navigation_search: {
-                    // Handle favorites click
-                    Intent intent = new Intent(this, MovieSearch.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                    finish();
-                    return true;
-                }
-                case R.id.navigation_trending: {
-                    // Handle settings click
-                    Intent intent = new Intent(this, Trending.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                    finish();
-                    return true;
-                }
-                case R.id.navigation_coming: {
-                    // Handle settings click
-                    Intent intent = new Intent(this, ComingSoon.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                    finish();
-                    return true;
+                return false;
             }
-            }
-            return false;
         });
     }
 }
