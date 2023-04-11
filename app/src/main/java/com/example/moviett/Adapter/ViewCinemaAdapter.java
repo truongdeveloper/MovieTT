27e0b7675f61077071a09fd583208d127e0150e7
dp-thao <thaodoan542001@gmail.com>
2023-04-11 16:12:57 +0700

    xoa cinemanow va cinemaupcoming

--- app/src/main/java/com/example/moviett/Adapter/ViewCinemaAdapter.java
+++ app/src/main/java/com/example/moviett/Adapter/ViewCinemaAdapter.java
@@ -5,14 +5,6 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-import com.example.moviett.Fragment.CinemaFragment;
-import com.example.moviett.Fragment.CinemaNowFragment;
-import com.example.moviett.Fragment.CinemaUpcomingFragment;
-import com.example.moviett.Fragment.ComingSoonFragment;
-import com.example.moviett.Fragment.HomeFragment;
-import com.example.moviett.Fragment.MovieSearchFragment;
-import com.example.moviett.Fragment.TrendingFragment;
-
 public class ViewCinemaAdapter extends FragmentPagerAdapter  {
     public ViewCinemaAdapter(@NonNull FragmentManager fm, int behavior) {
         super(fm, behavior);
