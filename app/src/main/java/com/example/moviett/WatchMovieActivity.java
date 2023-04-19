4c7c24276bcc2f14d60b59e418a1d94472ca1f90
truongdeveloper <vanminhtruong678@gmail.com>
2023-04-19 15:06:14 +0700

    pull code

--- app/src/main/java/com/example/moviett/WatchMovieActivity.java
+++ app/src/main/java/com/example/moviett/WatchMovieActivity.java
@@ -4,18 +4,28 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
 
 public class WatchMovieActivity extends AppCompatActivity {
 
-    WebView webView;
+    WebView webViewMovie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_movie);
+
+//        webViewMovie.setWebViewClient(new WebViewClient());
+//        webViewMovie.setWebChromeClient(new MyChrome());
 
         Intent intent = getIntent();
         int idMovie = intent.getIntExtra("idMovie", 1);
@@ -26,7 +36,7 @@ public class WatchMovieActivity extends AppCompatActivity {
         }
 
 
-        WebView webViewMovie = findViewById(R.id.webViewMovie);
+        webViewMovie = findViewById(R.id.webViewMovie);
         webViewMovie.getSettings().setJavaScriptEnabled(true);
         webViewMovie.getSettings().setDomStorageEnabled(true);
         webViewMovie.getSettings().setAllowFileAccess(true);
@@ -35,6 +45,16 @@ public class WatchMovieActivity extends AppCompatActivity {
         webViewMovie.getSettings().setAllowUniversalAccessFromFileURLs(true);
         webViewMovie.getSettings().setMediaPlaybackRequiresUserGesture(false);
         webViewMovie.loadUrl("https://www.2embed.to/embed/tmdb/movie?id=" + idMovie);
+
+        webViewMovie.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                // Xử lý url ở đây nếu cần thiết
+//                view.loadUrl(url);
+                return true;
+            }
+        });
     }
 
     @Override
@@ -45,4 +65,59 @@ public class WatchMovieActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+//    private class MyChrome extends WebChromeClient {
+//        private View mCustomView;
+//        private WebChromeClient.CustomViewCallback mCustomViewCallback;
+//        protected FrameLayout mFullscreenContainer;
+//        private int mOriginalOrientation;
+//        private int mOriginalSystemUiVisibility;
+//
+//        MyChrome() {}
+//
+//        public Bitmap getDefaultVideoPoster()
+//        {
+//            if (mCustomView == null) {
+//                return null;
+//            }
+//            return BitmapFactory.decodeResource(getApplicationContext().getResources(), 2130837573);
+//        }
+//
+//        public void onHideCustomView()
+//        {
+//            ((FrameLayout)getWindow().getDecorView()).removeView(this.mCustomView);
+//            this.mCustomView = null;
+//            getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+//            setRequestedOrientation(this.mOriginalOrientation);
+//            this.mCustomViewCallback.onCustomViewHidden();
+//            this.mCustomViewCallback = null;
+//        }
+//
+//        public void onShowCustomView(View paramView, WebChromeClient.CustomViewCallback paramCustomViewCallback)
+//        {
+//            if (this.mCustomView != null)
+//            {
+//                onHideCustomView();
+//                return;
+//            }
+//            this.mCustomView = paramView;
+//            this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+//            this.mOriginalOrientation = getRequestedOrientation();
+//            this.mCustomViewCallback = paramCustomViewCallback;
+//            ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
+//            getWindow().getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+//        }
+//    }
+//
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        webViewMovie.saveState(outState);
+//    }
+//
+//    @Override
+//    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        webViewMovie.restoreState(savedInstanceState);
+//    }
 }
